75b74659fc7167679cbf48a357332b3460f2c42c
David Kim <cjoak1028@gmail.com>
2022-11-20 18:02:49 -0500

    Added responsiveness to hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,11 +5,13 @@ import avatarImage from "./assets/IMG_0570.jpg";
 const Hero = () => {
   return (
     <div id={styles.hero}>
-      <h1>Yo.</h1>
-      <h3>I'm a Web Developer based in Toronto, Canada.</h3>
+      <div id={styles["hero-text"]}>
+        <h1>Yo.</h1>
+        <h3>I'm a Web Developer based in Toronto, Canada.</h3>
+      </div>
       <div id={styles["avatar-outline"]} className="flex flex-jc-c flex-ai-c">
         <div id={styles.avatar}>
-          <img src={avatarImage} alt="" />
+          <img src={avatarImage} alt="photo of CJ" />
         </div>
       </div>
     </div>
