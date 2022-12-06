864802c504023e386673a9ab1a4aca8c97c5849d
David Kim <cjoak1028@gmail.com>
2022-12-06 16:20:22 -0500

    Fixed contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,20 +3,22 @@ import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <div id={styles.contact}>
-      <div id={styles["contact-title"]} className="flex flex-ai-c mb-title">
+    <div className={styles.contact}>
+      <div className="flex flex-ai-c mb-title">
         <h2>CONTACT</h2>
         <hr />
       </div>
       <p>I’m looking for new opportunities, so feel free to contact me!</p>
-      <form id={styles["contact-form"]}>
-        <div id={styles["flex-wrapper"]}>
+      <form id="contactForm" className={styles["contact-form"]}>
+        <div className={styles["flex-wrapper"]}>
           <input type="text" id="name" name="name" placeholder="Name" />
           <input type="email" id="email" name="email" placeholder="Email" />
         </div>
         <input type="text" id="subject" name="subject" placeholder="Subject" />
         <textarea id="message" name="message" placeholder="Message" />
-        <button type="submit">Send</button>
+        <button className={styles["submit-btn"]} type="submit">
+          Send
+        </button>
       </form>
     </div>
   );
